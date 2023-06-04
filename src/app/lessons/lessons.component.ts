7fde61dadcdce7819692dc82bede27c739027f75
Kari Närhi <Kari.Narhi@tecinspire.com>
2023-06-04 14:34:45 +0300

    Disable undefined error for lessons-component observables.

--- src/app/lessons/lessons.component.ts
+++ src/app/lessons/lessons.component.ts
@@ -11,8 +11,8 @@ import { catchError } from "rxjs/operators";
   styleUrls: ["./lessons.component.css"],
 })
 export class LessonsComponent implements OnInit {
-  lessons$: Observable<Lesson[]>;
-  isLoggedIn$: Observable<boolean>;
+  lessons$!: Observable<Lesson[]>;
+  isLoggedIn$!: Observable<boolean>;
 
   constructor(private lessonsService: LessonsService) {}
 
