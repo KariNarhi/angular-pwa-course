d79ab94185a40778a0369e46b0c032a4ad8b5d5a
Kari Närhi <Kari.Narhi@tecinspire.com>
2023-06-04 14:08:17 +0300

    Added codes from old course base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { LessonsComponent } from "./lessons/lessons.component";
+
+import { LessonsService } from "./services/lessons.service";
+import { ReactiveFormsModule } from "@angular/forms";
+
+import { environment } from "../environments/environment.prod";
+import { ServiceWorkerModule } from "@angular/service-worker";
+
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { AppRoutingModule } from "./app-routing.module";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, LessonsComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [LessonsService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
